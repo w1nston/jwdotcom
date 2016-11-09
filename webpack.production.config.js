8f0bcb5d2df4808fbd0300b0765c4d6413d67958
w1nston <wallander.jonas@gmail.com>
2016-11-09 21:31:44 +0100

    Refactor webpack production config to minify reactjs.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,6 +13,11 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
